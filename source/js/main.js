f82c639847bef57dcfc6db1a4344378fe9350e95
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2025-02-18 15:25:16 +0300

    Добавляет блок Вопросы

--- source/js/main.js
+++ source/js/main.js
@@ -425,9 +425,22 @@ const initNewsSwiperCollection = () => {
   });
 };
 
+const initAccordionCollection = () => {
+  const accordionCollection = document.querySelectorAll('.accordion-item-js');
+
+  accordionCollection.forEach((accordion) => {
+    const input = accordion.querySelector('input');
+
+    input.addEventListener('input', () => {
+      accordion.classList.toggle('accordion-item--active');
+    });
+  });
+};
+
 initHeroSwiper();
 initMenu();
 initFeedbackModal();
 initFeedbackForm();
 initProgramsSwiper();
 initNewsSwiperCollection();
+initAccordionCollection();
