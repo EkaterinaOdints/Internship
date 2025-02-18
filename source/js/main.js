166fc6a22c844a18991f380a4c7481588b8afcf4
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2025-02-18 19:17:17 +0300

    Добавляет блок Отзывы

--- source/js/main.js
+++ source/js/main.js
@@ -437,6 +437,45 @@ const initAccordionCollection = () => {
   });
 };
 
+const initReviewsSwiper = () => {
+  new Swiper('.reviews-swiper-js', {
+    modules: [Navigation, Scrollbar],
+    direction: 'horizontal',
+    navigation: {
+      nextEl: '.reviews-swiper-button-next-js',
+      prevEl: '.reviews-swiper-button-prev-js',
+    },
+    scrollbar: {
+      el: '.reviews-swiper-scrollbar-js',
+      draggable: true,
+      enabled: false,
+      dragSize: 326,
+    },
+    slidesPerView: 'auto',
+    spaceBetween: 15,
+    breakpoints: {
+      768: {
+        slidesOffsetBefore: 45,
+        slidesOffsetAfter: 45,
+        spaceBetween: 30,
+        scrollbar: {
+          enabled: true,
+          dragSize: 326,
+        },
+      },
+      1440: {
+        slidesOffsetBefore: 0,
+        slidesOffsetAfter: 0,
+        spaceBetween: 32,
+        scrollbar: {
+          enabled: true,
+          dragSize: 394,
+        },
+      }
+    },
+  });
+};
+
 initHeroSwiper();
 initMenu();
 initFeedbackModal();
@@ -444,3 +483,4 @@ initFeedbackForm();
 initProgramsSwiper();
 initNewsSwiperCollection();
 initAccordionCollection();
+initReviewsSwiper();
